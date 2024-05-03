24eecf0b0658b23f8a969edde3643ec6c1fa217e
shibuambalathara <shibu@autobse.com>
2024-05-03 10:46:20 +0530

    changed the url and schema to autobse.in

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,16 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
  
  const config: CodegenConfig = {
-     schema: 'http://13.233.33.61/api/graphql',
+    //  schema: 'http://13.233.33.61/api/graphql',
 
 
   //  schema:'https://api-dev.autobse.com/api/graphql',
 
  //  schema:'https://api.autobse.com/api/graphql',
+
+//  schema:"http://localhost:3000/api/graphql",
+ schema:"https://autobse.in/api/graphql",
+
 
     documents: '**/*.graphql',
  
